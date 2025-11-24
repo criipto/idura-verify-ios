0f7dc4243ea1d06c5ca2f79162d8f931b03add2a
Jan Aagaard Meier <jan.meier@idura.eu>
2025-11-24 14:17:10 +0100

    Setup formatting and linting

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(
       targets: ["IduraVerify"]
     )
   ],
+  dependencies: [
+    .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.62.2")
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "IduraVerify"
     )
-
   ]
 )
